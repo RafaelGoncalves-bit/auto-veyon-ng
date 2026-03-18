4cf11b14ffaa17b1bac4255ac021d7853bcd584a
Rafael Gonçalves de Lima <83875972+RafaelGoncalves-bit@users.noreply.github.com>
2026-03-18 08:03:20 -0300

    Atualizando nome do arquivo "veyon-cli -> veyon-wcli"

--- windows/slave/install.bat
+++ windows/slave/install.bat
@@ -4,7 +4,7 @@ net session 1>NUL 2>NUL || (echo Need Admin. & Exit /b 1)
 
 veyon.exe /S /NoMaster /NoStartMenuFolder
 
-set veyon-cli="C:\Program Files\Veyon\veyon-cli.exe"
+set veyon-cli="C:\Program Files\Veyon\veyon-wcli.exe"
 %veyon-cli% config import confs.json
 
 "C:\Program Files\Python311\python.exe" -m pip install getmac
